c9558e6348a65e66ebd56890ee648b6ff50e48fa
tianzy <tianzy145@163.com>
2021-04-25 15:19:21 +0800

    425

--- common_libs/src/main/java/bean/CommonMenuItem.java
+++ common_libs/src/main/java/bean/CommonMenuItem.java
@@ -5,10 +5,26 @@ public class CommonMenuItem {
     private int icon;
     private String des;
     private int priority;
+    private String url;
+
 
     public CommonMenuItem(String name, int icon) {
         this.name = name;
         this.icon = icon;
+    }
+
+    public CommonMenuItem(String name, int icon, String url) {
+        this.name = name;
+        this.icon = icon;
+        this.url = url;
+    }
+
+    public CommonMenuItem(String name, int icon, String des, int priority, String url) {
+        this.name = name;
+        this.icon = icon;
+        this.des = des;
+        this.priority = priority;
+        this.url = url;
     }
 
     public CommonMenuItem(String name, int icon, int priority) {
@@ -22,6 +38,14 @@ public class CommonMenuItem {
         this.icon = icon;
         this.des = des;
         this.priority = priority;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 
     public String getName() {
